2af65e75d727654e3ac8f0f84d12aadecf440ffe
Soma Sekine <a8121151@aoyama.jp>
2024-06-26 19:36:03 +0900

    不要なコード・ファイル削除

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,5 @@
 import * as vscode from 'vscode';
-import showTest from './test';
 import WebSocket from 'ws';
-import { connect } from 'http2';
 import showSolved from './solvedButton';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -36,7 +34,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// 助けを求めるボタンが押された際の処理
 	const askForHelp = async () => {
-		// vscode.window.showInformationMessage('エラー情報を収集しています...');
 
 		let errorList: ErrorInfo[] = [];
 
@@ -69,26 +66,10 @@ export function activate(context: vscode.ExtensionContext) {
 			report += `  メンバー名: ${error.memberName}さん\n\n`;
 		});
 
-		// レポートを表示または送信
-		// if (errorList.length > 0) {
-		// 	// ここでレポートを使用して何かします（例：APIに送信、ファイルに保存など）
-		// 	vscode.window.showInformationMessage(report);
-		// 	// vscode.window.showInformationMessage(`${errorList.length} 件のエラーと警告の情報を収集しました。`);
-		// } else {
-		// 	vscode.window.showInformationMessage('現在、エラーや警告は検出されていません。');
-		// 	return;
-		// }
-
 		const sendMessage: () => void = () => ws.send("1");
 		showSolved(sendMessage);
 		sendErrorsToAPI(errorList[0]);
 	};
-
-	interface ErrorPayload {
-		memberName: string;
-		language: string;
-		message: string;
-	}
 
 	const ws = new WebSocket('ws://localhost:8080');
 
@@ -105,96 +86,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// APIに実際にエラー情報を送信する関数
 	async function sendErrorsToAPI(error: ErrorInfo): Promise<void> {
-		// ここでエラー情報をAPIに送信します
-		// const payloads: ErrorPayload = {
-		// 	memberName: error.memberName,
-		// 	language: error.language,
-		// 	message: error.message
-		// };
 
 		try {
 			ws.send(JSON.stringify({ source: 'vscode', memberName: error.memberName, language: error.language, message: error.message }));
 			console.log('send websocket error_message');
 		} catch (error) {
-			// if (axios.isAxiosError(error)) {
-			// 	vscode.window.showErrorMessage('APIエラー:', error.response?.data);
-			// } else {
-			// 	vscode.window.showErrorMessage('エラー:', error ?? '原因不明のエラー');
-			// }
 			vscode.window.showErrorMessage('エラー:', error ?? '原因不明のエラー');
 		}
 	}
 
-	// Simple notifications
-	const showInfoNotification = vscode.commands.registerCommand('notifications-sample.showInfo', () => {
-		vscode.window.showInformationMessage('Info Notification');
-	});
-
-	const showInfoNotificationAsModal = vscode.commands.registerCommand('notifications-sample.showInfoAsModal', () => {
-		vscode.window.showInformationMessage('Info Notification As Modal', { modal: true });
-	});
-
-	const showWarningNotification = vscode.commands.registerCommand('notifications-sample.showWarning', () => {
-		vscode.window.showWarningMessage('Warning Notification');
-	});
-
-	const showErrorNotification = vscode.commands.registerCommand('notifications-sample.showError', () => {
-		vscode.window.showErrorMessage('Error Notification');
-	});
-
-	// Notification with actions
-	const showWarningNotificationWithActions = vscode.commands.registerCommand('notifications-sample.showWarningWithActions', async () => {
-		const selection = await vscode.window.showWarningMessage('Warning Notification With Actions', 'Action 1', 'Action 2', 'Action 3');
-
-		if (selection !== undefined) {
-			vscode.window.showInformationMessage(`You selected: ${selection}`, { modal: true });
-		}
-	});
-
-	// Progress notification with option to cancel
-	const showProgressNotification = vscode.commands.registerCommand('notifications-sample.showProgress', () => {
-		vscode.window.withProgress({
-			location: vscode.ProgressLocation.Notification,
-			title: "Progress Notification",
-			cancellable: true
-		}, (progress, token) => {
-			token.onCancellationRequested(() => {
-				console.log("User canceled the long running operation");
-			});
-
-			progress.report({ increment: 0 });
-
-			setTimeout(() => {
-				progress.report({ increment: 10, message: "Still going..." });
-			}, 1000);
-
-			setTimeout(() => {
-				progress.report({ increment: 40, message: "Still going even more..." });
-			}, 2000);
-
-			setTimeout(() => {
-				progress.report({ increment: 50, message: "I am long running! - almost there..." });
-			}, 3000);
-
-			const p = new Promise<void>(resolve => {
-				setTimeout(() => {
-					resolve();
-				}, 5000);
-			});
-
-			return p;
-		});
-	});
-
-	// Show all notifications to show do not disturb behavior
-	const showAllNotifications = vscode.commands.registerCommand('notifications-sample.showAll', () => {
-		vscode.commands.executeCommand('notifications-sample.showInfo');
-		vscode.commands.executeCommand('notifications-sample.showWarning');
-		vscode.commands.executeCommand('notifications-sample.showWarningWithActions');
-		vscode.commands.executeCommand('notifications-sample.showError');
-		vscode.commands.executeCommand('notifications-sample.showProgress');
-		vscode.commands.executeCommand('notifications-sample.showInfoAsModal');
-	});
-
-	context.subscriptions.push(showInfoNotification, showInfoNotificationAsModal, showWarningNotification, showErrorNotification, showProgressNotification, showWarningNotificationWithActions, showAllNotifications, showTest, disposable);
+	context.subscriptions.push(disposable);
 }
--- src/test.ts
+++
@@ -1,7 +0,0 @@
-import * as vscode from 'vscode';
-
-const showTest = vscode.commands.registerCommand('notifications-sample.showTest', () => {
-    vscode.window.showInformationMessage('テストだよ');
-});
-
-export default showTest;
